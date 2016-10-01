63047752e56a5a90a4e86ac25693df35a1ffe5d3
elsayedawd <elsayedawd@elsayedawd-Inspiron-N5110>
2016-10-01 22:46:34 +0200

    print hello statment

--- InsightCrawler/src/main/java/com/mycompany/insightcrawler/CrawlerAdapter.java
+++ InsightCrawler/src/main/java/com/mycompany/insightcrawler/CrawlerAdapter.java
@@ -11,7 +11,7 @@ package com.mycompany.insightcrawler;
  */
 public class CrawlerAdapter {
     public static void main(String[] args) {
-        
+        System.out.println("Elsayed AWd ");
     }
     
 }
